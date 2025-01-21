44e43d4f2dacba2d979b9223ebb03c94853e5bf9
JensLotz <jl623@students.uni-freiburg.de>
2025-01-21 11:05:41 +0100

    Add files via upload

--- app.R
+++ app.R
@@ -3,35 +3,27 @@ library(leaflet)
 library(sf)
 library(dplyr)
 
-# Daten laden
-geo_data <- st_read("bbb.geojson")
+geo_data <- st_read("Wildfire.geojson")
 
-# Projektion überprüfen und transformieren
 if (st_crs(geo_data)$epsg != 4326) {
   geo_data <- st_transform(geo_data, crs = 4326)
 }
 
-# -999 Werte im Jahr entfernen
 geo_data <- geo_data %>% filter(YEAR != -999)
 
-
-# Koordinaten extrahieren
 coords <- st_coordinates(geo_data)
 geo_data <- cbind(geo_data, lon = coords[, 1], lat = coords[, 2])
 
 classify_size <- function(size) {
-  breaks <- c(seq(0, 100000, by = 10000), Inf) # 11 Intervalle
-  labels <- 1:(length(breaks) - 1) # Labels entsprechend den Intervallen
+  breaks <- c(seq(0, 100000, by = 10000), Inf) 
+  labels <- 1:(length(breaks) - 1) 
   cut(size, breaks = breaks, labels = labels, include.lowest = TRUE)
 }
 
-
-# Größe zuweisen
 geo_data$size_class <- as.numeric(classify_size(geo_data$SIZE_HA))
 
-# UI definieren
 ui <- fluidPage(
-  titlePanel("Interaktive Karte mit Filtern"),
+  titlePanel("Interaktive Waldbrandkarte von Kanada"),
   sidebarLayout(
     sidebarPanel(
       selectInput(
@@ -55,51 +47,64 @@ ui <- fluidPage(
         choices = c("Alle", sort(unique(geo_data$MONTH))),
         selected = "Alle",
         multiple = FALSE
+      ),
+      selectInput(
+        inputId = "province_filter",
+        label = "Provinz auswählen:",
+        choices = c("Alle", sort(unique(geo_data$SRC_AGENCY))),
+        selected = "Alle",
+        multiple = FALSE
       )
     ),
     mainPanel(
-      leafletOutput(outputId = "map")
+      leafletOutput(outputId = "map"),
+      br(),
+      textOutput("summary_text")
     )
   )
 )
 
-# Server definieren
 server <- function(input, output, session) {
   
-  # Gefilterte Daten
   filtered_data <- reactive({
     geo_data %>% 
       filter(
         (input$year_filter == "Alle" | YEAR == input$year_filter),
         (SIZE_HA >= input$size_filter[1] & SIZE_HA <= input$size_filter[2]) | 
           (input$size_filter[2] == 300000 & SIZE_HA > 300000),
-        (input$month_filter == "Alle" | MONTH == input$month_filter)
+        (input$month_filter == "Alle" | MONTH == input$month_filter),
+        (input$province_filter == "Alle" | SRC_AGENCY == input$province_filter)
       )
   })
   
-  # Karte rendern
   output$map <- renderLeaflet({
     leaflet() %>%
       addTiles() %>%
-      setView(lng = -106.3468, lat = 56.1304, zoom = 4) # Kanada zentrieren
+      setView(lng = -106.3468, lat = 56.1304, zoom = 4)  
   })
   
-  # Punkte aktualisieren
   observe({
     leafletProxy("map", data = filtered_data()) %>%
       clearMarkers() %>%
       addCircleMarkers(
         lng = ~lon,
         lat = ~lat,
-        radius = ~pmax(size_class * (7 / 10), 2), # Mindestgröße 3 Pixel
+        radius = ~pmax(size_class * (7 / 10), 2), 
         color = "darkred",
         stroke = FALSE,
         fillOpacity = 0.7,
-        popup = ~paste("<b>", FIRENAME, "</b><br>Provinz:", SRC_AGENCY, "<br>Größe:", SIZE_HA, "ha")
+        popup = ~paste("<b>", FIRENAME, "</b><br>Provinz:", SRC_AGENCY, "<br>Größe:", round(SIZE_HA / 100, 2), "km²")
       )
-    
+  })
+  
+  output$summary_text <- renderText({
+    data <- filtered_data()
+    total_fires <- nrow(data)
+    total_area_km2 <- sum(data$SIZE_HA / 100, na.rm = TRUE) 
+    paste("Gesamtanzahl Brände:", total_fires, 
+          " | Gesamte Brandfläche:", format(ceiling(total_area_km2), big.mark = ",", scientific = FALSE), "km²")
+
   })
 }
 
-# Shiny App starten
 shinyApp(ui = ui, server = server)
